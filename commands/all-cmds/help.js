f398efce55771a9f879c37bba1ac051486b98224
SHAHRIb <84126107+shahrib-hub@users.noreply.github.com>
2022-04-10 18:50:56 +0530

    Update help.js

--- commands/all-cmds/help.js
+++ commands/all-cmds/help.js
@@ -3,6 +3,10 @@ const disbutpages = require("discord-embeds-pages-buttons")
 const Discord = require("discord.js")
 const { button, MessageButton, MessageActionRow, MessageMenuOption, MessageMenu } = require('discord-buttons');
 const disbut = require("discord-buttons");
+      let days = Math.floor(client.uptime / 86400000);
+        let hours = Math.floor(client.uptime / 3600000) % 24;
+        let minutes = Math.floor(client.uptime / 60000) % 60;
+        let seconds = Math.floor(client.uptime / 1000) % 60;
 
     let embed1 = new Discord.MessageEmbed()
       .setTitle('**<a:Rainbow_heart:937584806923993128> Help Panel <a:Rainbow_heart:937584806923993128>**')
@@ -10,6 +14,7 @@ const disbut = require("discord-buttons");
       .setColor("#f4c2c2")
       .addField("<a:correct:937681819812392970> COMMAND CATEGORIES",
         "**<:atlanta_administration_category:789030159419310101>  ADMIN\n<:atlanta_general_category:789030444167200808>  GENERAL\n<a:Tada_Yellow:933244423234539560>  GIVEAWAY\n💙  INFO\n<a:money:908685366184271923> ECONOMY\n<:atlanta_images_category:789030084861362257>  IMAGE\n<:atlanta_fun_category:789030226607996958>  FUN\n<a:level_up:938072252778426409> LEVELLING\n<a:pin:908666657520619550> SLASH COMMANDS**")
+      .addField("Uptime","${days}days ${minutes}min ${seconds}sec
   
       .setFooter('HEAven Guarder | Prefix: !h')
     let embed2 = new Discord.MessageEmbed()
