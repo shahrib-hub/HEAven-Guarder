8671bddc9958bdd871bd84ef08362242be477c02
SHAHRIb <84126107+shahrib-hub@users.noreply.github.com>
2022-10-31 10:41:51 +0530

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports.run = async (client) => {
   console.log(`${client.user.tag} has logged in.`);
-  client.user.setActivity(`Prefix + | Made by SHAHRIb`, {
+  client.user.setActivity(`-help | @Nektar`, {
     type: "ONLINE",
   });
 };
